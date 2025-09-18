d202b71da41bbf1f7d07aa04085873dde2802053
ttoksas <58305934+ttunahanoksas@users.noreply.github.com>
2025-09-18 11:38:26 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-#include <math.h> //Why do we need this library?
+#include <math.h> 
 
-int main()
-{
-    // declare variables
-
-    // get user input
-
-    /* calculate the discriminant   <-- this is a comment for multiple lines
-     the discriminant is: b² - 4ac
-    */
-
-    // according to the value of the discriminant, calculate the roots and print the results
-
-    return 0;
+int main(){
+    double a,b,c;
+    double discriminant, x1, x2;
+    printf("Enter coefficients a, b, and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a==0){
+        printf("This is not a quadratic equation");
+    }
+    discriminant = b*b-4*a*c;
+    if (discriminant > 0){
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Two real roots: x1 = %.4f, x2 = %.4f\n", x1, x2);
+    }else if (discriminant==0){
+        x1 = -b / (2 * a);
+         printf("One real root: x = %.4f\n", x1);
+    }
+    else{
+    
+    printf("There is no real root in this equation");
+    }
+         
+    
 }
