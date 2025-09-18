b76a4dbec994dfd583946b347522103c79c63083
ttoksas <58305934+ttunahanoksas@users.noreply.github.com>
2025-09-18 11:41:24 +0200

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,11 +1,11 @@
-#include <studio.h>;
+#include <stdio.h>
 
-int main{} {
+int main (){
     int x;
 
     printf("Dear user!");
     printf("Enter a number, I will print its square!");
-    scanf("%d", x);
-    x = x*x
+    scanf("%f", x);
+    x = x*x;
     printf("x square is: %f", x);
 }
